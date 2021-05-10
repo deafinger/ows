43f697ae67a5c30b69b29ba315b07eff18516172
solomon0215 <50070831+solomon0215@users.noreply.github.com>
2021-05-10 18:12:31 +0900

    2021-05-10 test

--- src/main/java/com/api/ows/reservation/service/FutureBookingSummaryService.java
+++ src/main/java/com/api/ows/reservation/service/FutureBookingSummaryService.java
@@ -5,20 +5,7 @@ import java.util.Map;
 import com.api.ows.reservation.vo.request.FutureBookingSummaryReqVO;
 import com.api.ows.reservation.vo.response.FutureBookingSummaryResVO;
 
-/**
- * @Class FutureBookingSummaryService.java
- * @Description : FutureBookingSummary OWS ��� ���� service
- * @
- * @ ������      	     ������           ��������
- * @ ---------  	 ---------   	-------------------------------
- * @ 2021. 5. 6.     ������     		���ʻ���
- *
- * @author ������
- * @since 2021. 5. 6.
- * @version 1.0
- *
- *  Copyright (��)���Ӱ���Ʈ
- */
+
 public interface FutureBookingSummaryService {
 	/**
 	* @param  FutureBookingSummaryReqVO
